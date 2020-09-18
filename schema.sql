aafcd4dad6a7cbdc85c3fa909a6f1a439588e7e5
Czarl Jalos <czarlfrancis@gmail.com>
2020-09-18 01:41:57 -0700

    added table for weather

--- schema.sql
+++ schema.sql
@@ -6,4 +6,11 @@ CREATE TABLE location (
   formatted_query VARCHAR(255),
   latitude VARCHAR(255),
   longitude VARCHAR(255)
-)
+);
+DROP TABLE IF EXISTS weather;
+CREATE TABLE weather (
+  id SERIAL PRIMARY KEY,
+  forecast VARCHAR(255),
+  time VARCHAR(255),
+  city VARCHAR(255)
+);
